8d40c74f486cb1680eea3c0f86932d45a23be792
agamealion <105758032+agamealion@users.noreply.github.com>
2023-07-03 16:03:39 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,19 @@
 <?php
-phpinfo();
+$host = "aws.connect.psdb.cloud";
+$username = "wwiyhcrpt5ud4j4aj7mt";
+$password = "pscale_pw_GK6q3vfp7U2G4lOOwwHA6HGAx7pO8KZxWbzKoavGxG2";
+$database = "lisai";
+
+// 建立连接
+$conn = new mysqli($host, $username, $password, $database);
+
+// 检查连接是否成功
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} else {
+    echo "连接成功";
+}
+
+// 关闭连接
+$conn->close();
+?>
