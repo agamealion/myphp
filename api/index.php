a5ee7486a3d9051b3454eea8e141ab35ccaf64e4
agamealion <105758032+agamealion@users.noreply.github.com>
2023-07-03 16:10:25 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,8 +4,17 @@ $username = "wwiyhcrpt5ud4j4aj7mt";
 $password = "pscale_pw_GK6q3vfp7U2G4lOOwwHA6HGAx7pO8KZxWbzKoavGxG2";
 $database = "lisai";
 
+// 配置 SSL/TLS 选项
+$ssl_options = array(
+    "ssl" => array(
+        "verify_peer" => false,
+        "verify_peer_name" => false,
+        "allow_self_signed" => true
+    )
+);
+
 // 建立连接
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($host, $username, $password, $database, null, $ssl_options);
 
 // 检查连接是否成功
 if ($conn->connect_error) {
